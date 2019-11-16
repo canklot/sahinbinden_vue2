75d3f03ffd9daaad9e154bac7e05a62dbc853911
canklot <canklot@users.noreply.github.com>
2019-11-16 23:23:13 +0300

    Header ismi footer oldu. isim karışıklığı olmuş ya

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Header from './Header.vue'
+import Footer from './Footer.vue'
 
 
-new Vue({
-  el: '#Header',
-  render: h => h(Header)
-})
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+new Vue({
+  el: '#Footer',
+  render: h => h(Footer)
 })
